2b13b6bcf1d651d22d2f4d99f7b29c9c86aa1e27
vinprograms <vincenttrinh94@gmail.com>
2024-01-31 12:34:26 -0800

    added data search

--- src/App.js
+++ src/App.js
@@ -66,11 +66,12 @@ const theme: Theme = {
 				color: { value: '{colors.red.60}' },
 			  },
 			  _focus: {
-				color: { value: '{colors.red.60}' },
+				color: { value: '{colors.red.40}' },
+				borderColor: { value: '{colors.red.60}'}, 
 			  },
 			  _active: {
 				color: { value: '{colors.red.60}' },
-				borderColor: { value: '{colors.red.80}' },
+				borderColor: { value: '{colors.red.60}' },
 				backgroundColor: { value: '{colors.neutral.40}' },
 			  },
 			  _disabled: {
@@ -172,6 +173,14 @@ const App = ({ signOut }) => {
 	
 	const [customers, setCustomers] = useState([]);
 	const [devices, setDevices] = useState([]);
+	
+	const [samples, setSamples] = useState([]); // set samples as state variable and tie it to setSamples() function
+	const [showTables, setShowTables] = useState(false);
+	const [validIds, setIds] = useState([]);
+
+	useEffect(() => { // call fetchSamples() once on render
+		fetchSamples();
+	  }, []);
 
 	useEffect(() => {
 		generateCustomers();
@@ -218,12 +227,224 @@ const App = ({ signOut }) => {
 		setDevices(devices);
 	}
 
-	return (
+	function parseData(data) {
+		  data = data.replace(/[\"{}]/g, ''); // remove special characters that are part of JSON string
+		  const splitData = data.split(','); // tokenize string to create array of measurements
+		  const dataMap = new Map();
+		  splitData.map(data => { // iterate through tokenized string and create map of ["MEASUREMENT TYPE" => VALUE]
+			  if (data.match(/T:/)) {
+				  data = data.replace(/T:/, '');
+				  dataMap.set("T", data);
+			  }
+			  else if (data.match(/V1:/)) {
+				  data = data.replace(/V1:/, '');
+				  dataMap.set("V1", data);
+			  }
+			  else if (data.match(/V2:/)) {
+				  data = data.replace(/V2:/, '');
+				  dataMap.set("V2", data);
+			  }
+			  else if (data.match(/V3:/)) {
+				  data = data.replace(/V3:/, '');
+				  dataMap.set("V3", data);
+			  }
+			  else if (data.match(/I1:/)) {
+				  data = data.replace(/I1:/, '');
+				  dataMap.set("I1", data);
+			  }
+			  else if (data.match(/I2:/)) {
+				  data = data.replace(/I2:/, '');
+				  dataMap.set("I2", data);
+			  }
+			  else if (data.match(/I3:/)) {
+				  data = data.replace(/I3:/, '');
+				  dataMap.set("I3", data);
+			  }
+			  else if (data.match(/In:/i)) {
+				  data = data.replace(/In:/i, '');
+				  dataMap.set("In", data);
+			  }
+			  else if (data.match(/W1:/)) {
+				  data = data.replace(/W1:/, '');
+				  dataMap.set("W1", data);
+			  }
+			  else if (data.match(/W2:/)) {
+				  data = data.replace(/W2:/, '');
+				  dataMap.set("W2", data);
+			  }
+			  else if (data.match(/W3:/)) {
+				  data = data.replace(/W3:/, '');
+				  dataMap.set("W3", data);
+			  }
+			  else if (data.match(/Wt:/)) {
+				  data = data.replace(/Wt:/, '');
+				  dataMap.set("Wt", data);
+			  }
+		  });
+		  return dataMap;
+	  }
+	  
+	  function parseSample(samples, myMap) { // create a map of <id, array>
+		  samples.map(sample => {
+			  if (myMap.get(sample.device_id) === undefined) { // if map doesn't contain meter id yet, initialize with <id, array[map]>
+				const dataArr = [];
+				myMap.set(sample.device_id, dataArr); // elements are of the form <ID, array[map<measurement,data>]>
+			  }
+			  myMap.get(sample.device_id).push(parseData(sample.device_data)); // add map of measurements to map array
+		  });
+	  }
+
+	  async function fetchSamples() {
+		try {
+			const apiData = await API.graphql({ query: listSamples }, {limit: 1000});
+			const samplesFromAPI = apiData.data.listSamples.items;
+			const sampleMap = new Map();
+			parseSample(samplesFromAPI, sampleMap);
+			console.log("samples = ", sampleMap);
+			setSamples(sampleMap);
+		} catch (error) {
+			console.log('Error on fetching samples: ', error);
+		}
+	  }
+
+	  function validateId(ids) {
+		  try {
+			  ids = ids.replace(/ /g, ''); // remove spaces
+			  ids = ids.split(/,/); // tokenize string to create array of ids
+			  let myHTML = '';
+			  let myArr = [];
+			  let match = false;
+			  ids.map(id => {
+				  if (samples.get(id) !== undefined) {
+					match = true; // check to see if we have a match first, so we can make container for tables
+					setShowTables(true); // set state to show tables
+					myArr.push(id);
+				  }
+				  else {
+					alert('Failed to find the id you entered. Please check your entry and try again.')
+				  }
+				  if (match) {
+					  setIds(myArr); // store the valid ids in state
+				  }
+			  });
+		  } catch (error) {
+			  console.log('Error on validating id(s): ', error);
+		  }
+	  }
+	  
+		function makeTables(id) {
+			return (
+				<div>
+					<p>Status: Online</p>
+					<p>Location: Santa Clara, CA</p>
+					<p>Total number of records: {samples.get(id).length}</p>
+					<Table highlightOnHover variation="bordered">
+						<TableHead>
+							<TableRow>
+								<TableCell as="th">Sample</TableCell>
+								<TableCell as="th">Time</TableCell>
+								<TableCell as="th">V1</TableCell>
+								<TableCell as="th">V2</TableCell>
+								<TableCell as="th">V3</TableCell>
+								<TableCell as="th">I1</TableCell>
+								<TableCell as="th">I2</TableCell>
+								<TableCell as="th">I3</TableCell>
+								<TableCell as="th">In</TableCell>
+								<TableCell as="th">W1</TableCell>
+								<TableCell as="th">W2</TableCell>
+								<TableCell as="th">W3</TableCell>
+								<TableCell as="th">Wt</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+						{
+							samples.get(id).map((sample, i) => {  // use samples [id => measurements] map to generate table data
+								return (
+									<TableRow>
+										<TableCell>
+										{i+1}
+										</TableCell>
+										<TableCell>
+										{sample.get("T")}
+										</TableCell>
+										<TableCell>
+										{sample.get("V1")}
+										</TableCell>
+										<TableCell>
+										{sample.get("V2")}
+										</TableCell>
+										<TableCell>
+										{sample.get("V3")}
+										</TableCell>
+										<TableCell>
+										{sample.get("I1")}
+										</TableCell>
+										<TableCell>
+										{sample.get("I2")}
+										</TableCell>
+										<TableCell>
+										{sample.get("I3")}
+										</TableCell>
+										<TableCell>
+										{sample.get("In")}
+										</TableCell>
+										<TableCell>
+										{sample.get("W1")}
+										</TableCell>
+										<TableCell>
+										{sample.get("W2")}
+										</TableCell>
+										<TableCell>
+										{sample.get("W3")}
+										</TableCell>
+										<TableCell>
+										{sample.get("Wt")}
+										</TableCell>
+									</TableRow>
+								)
+							})
+						}
+						</TableBody>
+					</Table>
+				</div>
+			)
+		}
+	  
+	  function makeTabs() {
+		  return (
+			<div className="margin-med">
+				<Heading className="heading-blue" level={4}>Meter ID:</Heading>
+				<Tabs.Container defaultValue="Tab 0" isLazy>
+					<Tabs.List spacing="equal" justifyContent="center" indicatorPosition="top">
+						{
+							validIds.map((id, i) => { // create tab buttons
+								return ( 
+									<Tabs.Item value={'Tab ' + i} key={i}>
+										{id}
+									</Tabs.Item>
+								)
+							})
+						}
+					  </Tabs.List>
+						{
+							validIds.map((id, i) => { // create tab content
+								return (
+									<Tabs.Panel value={'Tab ' + i} key={i}>
+										{makeTables(id)}
+									</Tabs.Panel>
+								)
+							})
+						}
+				</Tabs.Container>
+			</div>
+		  );
+	  }
+	  
+	  return (
 		<ThemeProvider theme={theme}>
-			
-			<div className="page-margin black-bg horizontal" style={{padding: '0.75em 0em 0.75em 0em'}}>
+		<div className="page-margin black-bg horizontal" style={{padding: '0.75em 0em 0.75em 0em'}}>
 				<img style={{marginLeft: '0.25em'}} src={pslogo} alt="PowerSight Logo" />
-				<Heading level={4}>Administrator Console</Heading>
+				<Heading level={4}>Administrator Console, Version 1.0</Heading>
 			</div>
 			<Card className="page-margin" variation="outlined">
 				<div className="margin-small" style={{display: 'flex'}}>
@@ -298,12 +519,40 @@ const App = ({ signOut }) => {
 					</Table>
 				</div>
 			</Card>
+			<Card className="page-margin" variation="outlined">
+				<div className="margin-small">
+					<Heading className="heading-blue" level={4}>View Data Records:</Heading>
+					<p className="margin-small">Please enter your meter id(s). Separate by commas for multiple inputs:</p>
+					<Flex>
+						<TextField
+							placeholder="00000, 00001, 00002"
+							onKeyPress={(e) => {
+								if (e.key === 'Enter') {
+									validateId(e.currentTarget.value); // check if user input corresponds to valid meter ids
+								}
+							}}
+							innerEndComponent={ // generate magnifying glass button
+								<FieldGroupIconButton
+								  ariaLabel="Search"
+								  variation="link"
+								  onClick={() => alert('Still developing, please press enter for now.')}
+								>
+								  <MdSearch />
+								</FieldGroupIconButton>
+							}
+						/>
+					</Flex>
+				</div>
+				{
+					showTables && validIds.length && makeTabs() // check for showTables state and that we have hits for user entered ids
+				}
+			</Card>
 			<div className="margin-med center">
 				<Button variation="primary" size="large" onClick={signOut}>Sign Out</Button>
 			</div>
 		</ThemeProvider>
 	  );
-};
+	};
 
 export default withAuthenticator(App);
 
